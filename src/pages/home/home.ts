8be87d0aea6e31401cecc6b306d2c6f660195ed3
Rodolfo Almeida Marques <rodolfoalmeidamarques@gmail.com>
2018-09-28 15:24:54 +0100

    Updated home page to show a list of users - v1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,25 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	public users;
 
-  }
+	constructor( public navCtrl: NavController ) {}
+
+	ionViewDidLoad() {
+		this.users = [
+			{ name: 'Example 1', email: 'one@example.com', street_name: 'some address 1', city: 'Dublin', zip_code: 'A00B0C0', company_name: 'Accela' },
+			{ name: 'Example 2', email: 'two@example.com', street_name: 'some address 2', city: 'Galway', zip_code: 'A11B1C1', company_name: 'Accela' },
+			{ name: 'Example 3', email: 'three@example.com', street_name: 'some address 3', city: 'Cork', zip_code: 'A22B2C2', company_name: 'Accela' }
+		];
+	}
+
+	viewItem(){
+
+	}
 
 }
