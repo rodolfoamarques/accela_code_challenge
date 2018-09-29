8576b96747d11c6e6333323e789a165428382b8a
Rodolfo Almeida Marques <rodolfoalmeidamarques@gmail.com>
2018-09-29 13:13:03 +0100

    Added support for google maps

--- src/pages/user-detail/user-detail.ts
+++ src/pages/user-detail/user-detail.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { User } from './../../providers/users-api/users-api';
+
+declare var google;
 
 
 
@@ -12,12 +14,32 @@ import { User } from './../../providers/users-api/users-api';
 })
 export class UserDetailPage {
 
+	@ViewChild('map') mapElement: ElementRef;
+
+	private map: any;
 	private user: User;
 
 	constructor( public navCtrl: NavController, public navParams: NavParams ) {}
 
 	ionViewDidLoad() {
 		this.user = this.navParams.get( 'user' );
+    	this.initMap();
+	}
+
+	initMap() {
+		const mapOptions = {
+			center: { lat: Number(this.user.address.geo.lat), lng: Number(this.user.address.geo.lng) },
+			disableDefaultUI: true,
+			fullscreenControl: false,
+			mapTypeControl: false,
+			rotateControl: false,
+			scaleControl: false,
+			streetViewControl: false,
+			zoom: 7,
+			zoomControl: false
+		};
+
+		this.map = new google.maps.Map( this.mapElement.nativeElement, mapOptions );
 	}
 
 	phoneCall() {
